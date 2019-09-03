0050e57e8d5c2caecb99677f2eb2530a3bfef266
AmberWebb <amberrw@yahoo.com>
2019-09-02 21:41:43 -0700

    Added bands in town Api as well as concert this. This part was the most challenging because I couldn't get the right Api. but finally did.

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
+const readline = require('readline')
+const fs = require('fs')
 const axios = require('axios')
+var term = require( 'terminal-kit' ).terminal ;
+
 var player = require('play-sound') (opts = {})
 
 var Spotify = require('node-spotify-api');
@@ -16,11 +20,26 @@ var commandToRun = process.argv[3]
 
 if(functionToRun=='concert-this'){
     var command = commandToRun
+
+
+
     function concertThis(command){
         //make variable equal to generated url like so https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
         var url = "https://rest.bandsintown.com/artists/" + command + "/events?app_id=codingbootcamp"
         //make a post or get requet to the url above using jquery and ajax
-
+        const instance = axios.create({
+            baseURL: url, 
+            
+        })
+        instance.get('', {
+            
+        })
+        .then(function(response){
+            console.log("Concert This: ", response.data)
+        })
+        .catch(function(err){
+            console.log(err)
+        })
         //Get concert data and open it in your browser
         console.log("You want me to find concerts for this song:", command)
         
@@ -93,7 +112,95 @@ if(functionToRun=='movie-this'){
 
 if(functionToRun=='do-what-it-says'){
     var command = commandToRun
+
+    var arrayOfLines = []
+
+    fs.readFile('random.txt', function(err,data){
+        if(err) throw err
+        arrayOfLines = data.toString().split("\n")
+        var numberOfLines = arrayOfLines.length
+        console.log("Number of Lines: ",numberOfLines)
+        var randomLine = Math.floor(Math.random()*numberOfLines)
+        console.log("Array of Lines: ",arrayOfLines)
+        console.log("Random Line: ",randomLine)
+        var randomCommand = arrayOfLines[randomLine]
+        console.log('Random Command Issued: ',randomCommand)
+        var splitCommand = randomCommand.split(' ')
+
+        if(splitCommand[0]=='movie-this'){
+            
+            var command = splitCommand[1]
+    function movieThis(command){
+        const instance = axios.create({
+            baseURL: 'http://www.omdbapi.com/?apikey=697e013a', 
+            
+        })
+        instance.get('', {
+            params:{
+                t:command
+            }
+        })
+        .then(function(response){
+            console.log("Movie Response: ", response.data)
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+        console.log("You want me to find a movie for:", command)
+    }
+    movieThis(command)
+        }
+        
+        if(splitCommand[0]=='spotify-this'){
+            var songTitleArray = splitCommand.splice(1,5).join(" ")
+            console.log("Song Title Array: ",songTitleArray)
+            var command = songTitleArray
+
+            function spotifyThis(command){
+                console.log("You want me to spotify this:", command)
+        
+                spotify.search({ type: 'track', query: command }, function(err, data) {
+                    if (err) {
+                      return console.log('Error occurred: ' + err);
+                    }
+                   
+                    if (data.tracks.total==0){
+                        spotify.search({ type: 'track', query: 'The Sign by Ace of Base', limit:1 }, function(err, data) {
+                            if (err) {
+                              return console.log('Error occurred: ' + err);
+                            }
+                            console.log("Album: ", data.tracks.items[0].album.name ,"Song Title: ",data.tracks.items[0].name, "Preview: ",JSON.stringify(data.tracks.items[i].preview_url))
+                            var artistArray = []
+                    for (var A=0;A<data.tracks.items[i].artists.length;A++){
+                        artistArray.push(data.tracks.items[i].artists[A].name)
+                    }
+                    console.log ("Artist(s) on this Album: ",JSON.stringify(artistArray) +"\n" )
+                        })
+                    }
+        
+                  for ( var i =0;i<data.tracks.items.length;i++){
+                   var number = i+1   
+                    console.log ("Album "+number+":",JSON.stringify(data.tracks.items[i].album.name), "Song: ",JSON.stringify(data.tracks.items[i].name), "Preview: ",JSON.stringify(data.tracks.items[i].preview_url))
+                    var artistArray = []
+                    for (var A=0;A<data.tracks.items[i].artists.length;A++){
+                        artistArray.push(data.tracks.items[i].artists[A].name)
+                    }
+                    console.log ("Artist(s) on this Album: ",JSON.stringify(artistArray) +"\n" )
+        
+                  }
+                  });
+            }
+            spotifyThis(songTitleArray)
+        }
+
+
+
+    })
+
+    
+
     function doThis(command){
+        
         console.log("You want me to do:", command)
     }
     doThis(command)
